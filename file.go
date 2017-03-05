af46a48c17899acd5e37085d845af3c9c05a4afc
Mark Bates <mark@markbates.com>
2017-03-05 17:27:52 -0500

    Initial Commit

---
+++ file.go
@@ -0,0 +1,84 @@
+package makr
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"os"
+	"path/filepath"
+
+	"github.com/pkg/errors"
+)
+
+// File to generate
+type File struct {
+	Path          string
+	Template      string
+	TemplateFuncs template.FuncMap
+	Permission    os.FileMode
+	Should        ShouldFunc
+}
+
+// Run the generator
+func (f *File) Run(rootPath string, data Data) error {
+	return chdir(rootPath, func() error {
+		if f.Should != nil && !f.Should(data) {
+			return nil
+		}
+
+		path, err := f.render(f.Path, data)
+		if err != nil {
+			return err
+		}
+
+		body, err := f.render(f.Template, data)
+		if err != nil {
+			return err
+		}
+
+		return f.save(rootPath, path, body)
+	})
+}
+
+func (f *File) save(rootPath, path, body string) error {
+	dir := filepath.Dir(path)
+	err := os.MkdirAll(filepath.Join(rootPath, dir), 0755)
+	if err != nil {
+		return err
+	}
+
+	odir := filepath.Join(rootPath, path)
+	fmt.Printf("--> %s\n", odir)
+
+	ff, err := os.Create(odir)
+	if err != nil {
+		return err
+	}
+
+	_, err = ff.WriteString(body)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	return nil
+}
+
+func (f *File) render(s string, data Data) (string, error) {
+	t, err := template.New(s).Parse(s)
+	t = t.Funcs(f.TemplateFuncs)
+	bb := &bytes.Buffer{}
+	err = t.Execute(bb, data)
+	return bb.String(), err
+}
+
+// NewFile set up with sensible defaults
+func NewFile(path string, t string) *File {
+	return &File{
+		Path:          path,
+		Template:      t,
+		TemplateFuncs: Helpers,
+		Permission:    0664,
+		Should: func(data Data) bool {
+			return true
+		},
+	}
+}
---
+++ makr.go
@@ -0,0 +1,82 @@
+package makr
+
+import (
+	"context"
+	"os"
+
+	"github.com/pkg/errors"
+)
+
+// Data to be passed into generators
+type Data map[string]interface{}
+
+// ShouldFunc decides whether a generator should be run or not
+type ShouldFunc func(Data) bool
+
+// Runnable interface must be implemented to be considered a runnable generator
+type Runnable interface {
+	Run(string, Data) error
+}
+
+// Generator is the top level construct that holds all of the Runnables
+type Generator struct {
+	Runners []Runnable
+	Should  ShouldFunc
+}
+
+// New Generator
+func New() *Generator {
+	return &Generator{
+		Runners: []Runnable{},
+	}
+}
+
+// Add a Runnable generator to the list
+func (g *Generator) Add(r Runnable) {
+	g.Runners = append(g.Runners, r)
+}
+
+// Run all of the generators
+func (g *Generator) Run(rootPath string, data Data) error {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	return chdir(rootPath, func() error {
+		if g.Should != nil {
+			b := g.Should(data)
+			if !b {
+				return nil
+			}
+		}
+		err := os.MkdirAll(rootPath, 0755)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+		err = os.Chdir(rootPath)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+		for _, r := range g.Runners {
+			select {
+			case <-ctx.Done():
+				break
+			default:
+				err := r.Run(rootPath, data)
+				if err != nil {
+					return errors.WithStack(err)
+				}
+			}
+		}
+		return nil
+	})
+}
+
+func chdir(path string, fn func() error) error {
+	pwd, _ := os.Getwd()
+	defer os.Chdir(pwd)
+	os.Chdir(path)
+	return fn()
+}
+
+var nullShould = func(data Data) bool {
+	return true
+}
