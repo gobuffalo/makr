b741e6545d4605d7d8595423a00b89bdfed3572d
Mark Bates <mark@markbates.com>
2017-07-25 14:38:21 -0400

    override the default `go` path with ENV

--- go_commands.go
+++ go_commands.go
@@ -1,19 +1,23 @@
 package makr
 
-import "os/exec"
+import (
+	"os/exec"
+
+	"github.com/gobuffalo/envy"
+)
 
 // GoInstall compiles and installs packages and dependencies
 func GoInstall(pkg string, opts ...string) *exec.Cmd {
 	args := append([]string{"install"}, opts...)
 	args = append(args, pkg)
-	return exec.Command("go", args...)
+	return exec.Command(envy.Get("GO_BIN", "go"), args...)
 }
 
 // GoGet downloads and installs packages and dependencies
 func GoGet(pkg string, opts ...string) *exec.Cmd {
 	args := append([]string{"get"}, opts...)
 	args = append(args, pkg)
-	return exec.Command("go", args...)
+	return exec.Command(envy.Get("GO_BIN", "go"), args...)
 }
 
 // GoFmt is command that will use `goimports` if available,
